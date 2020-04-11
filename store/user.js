6b0f0a120d2968b6fb2857a063d4585c48710fd8
Raja s <rajaaltus@gmail.com>
2020-04-11 18:10:06 +0700

    login and register updated

--- store/user.js
+++ store/user.js
@@ -4,18 +4,22 @@ export const state = () => ({
 	strategy: "local",
 	user: false,
 	fullUser: {},
-	avatarUrl: null,
-	userProfile: {
-		success: false,
-		result: [],
-		error: {},
-	},
+	userProfile: {},
 	departments: {
 		success: false,
 		result: [],
 		error: {},
 	},
-	
+	usersList: {
+		success: false,
+		result: [],
+		error: {},
+	},
+	activeUsersList: {
+		success: false,
+		result: [],
+		error: {},
+	}
 });
 
 export const getters = {
@@ -29,14 +33,74 @@ export const getters = {
 };
 
 export const mutations = {
+	SET_USERPROFILE (state, userProfile) {
+		state.userProfile = userProfile;
+	},
 	SET_FULL_USER (state, fullUser) {
 		state.fullUser = fullUser;
 		state.avatarUrl = fullUser.avatar.url;
+	},
+	SET_DEPT_DATA (state, departments) {
+		if (departments && Array.isArray(departments) ) {
+			state.departments.success = true;
+			state.departments.result = departments;
+			state.departments.error = {};
+		} else {
+			// console.log('error'+departments);
+			state.departments.success = false;
+			state.departments.error = {
+				message: {
+					message: 'Error occured!'
+				}
+			};
+		}
+	},
+	SET_USERS_LIST (state, usersList) {
+		if (usersList && Array.isArray(usersList)) {
+			state.usersList.success = true;
+			state.usersList.result = usersList;
+			state.usersList.error = {};
+		} else {
+			state.usersList.success = false;
+			state.usersList.error = {
+				message: {
+					message: 'Error occured!'
+				}
+			};
+		}
+	},
+	SET_ACTIVE_USERS_LIST (state, activeUsersList) {
+		if (activeUsersList && Array.isArray(activeUsersList)) {
+			state.activeUsersList.success = true;
+			state.activeUsersList.result = activeUsersList;
+			state.activeUsersList.error = {};
+		} else {
+			state.activeUsersList.success = false;
+			state.activeUsersList.error = {
+				message: {
+					message: 'Error occured!'
+				}
+			};
+		}
 	}
 };
 
 
 export const actions = {
+	async addProfile ({commit}, payload) {
+		await this.$axios.$post('/user-profiles', payload)
+			.then(response =>  {
+			// handle success
+				commit("SET_USERPROFILE", response);
+			})
+			.catch((e) => {
+			// handle error
+				// commit("SET_USERPROFILE", error);
+			})
+			.finally(function () {
+			// always executed
+			});
+	},
 	async setFullUser ({commit}, {id}) {
 		await this.$axios.$get(`/users/${id}`)
 			.then(response =>  {
@@ -51,7 +115,80 @@ export const actions = {
 			// always executed
 				// console.log('finally');
 			});
+	},
+	async updateUserProfile ({commit}, payload) {
+		await this.$axios.$put(`/user-profiles/${payload.id}`, payload)
+			.then(response =>  {
+			// handle success
+				commit("SET_USERPROFILE", response);
+			})
+			.catch((e) => {
+			// handle error
+				// commit("SET_USERPROFILE", error);
+			})
+			.finally(function () {
+			// always executed
+			});
+	},
+	async setUsersList ({commit}, {qs}) {
+		await this.$axios.$get(`/users?${qs}`)
+			.then(response =>  {
+			// handle success
+				commit("SET_USERS_LIST", response);
+			})
+			.catch((e) => {
+			// handle error
+				// commit("SET_USERS_LIST", error);
+			})
+			.finally(function () {
+			// always executed
+				// console.log('finally');
+			});
+	},
+	async setActiveUsersList ({commit}, {qs}) {
+		await this.$axios.$get(`/users?${qs}`)
+			.then(response =>  {
+			// handle success
+				commit("SET_ACTIVE_USERS_LIST", response);
+			})
+			.catch((e) => {
+			// handle error
+				// commit("SET_ACTIVE_USERS_LIST", error);
+			})
+			.finally(function () {
+			// always executed
+				// console.log('finally');
+			});
+	},
+
+	async getAllDepartment ({commit}, departments) {
+		await this.$axios.$get('/departments') 
+			.then(response =>  {
+			// handle success
+				commit("SET_DEPT_DATA", response);
+			})
+			.catch((e) => {
+			// handle error
+				// commit("SET_DEPT_DATA", error);
+			})
+			.finally(function () {
+			// always executed
+				// console.log('finally');
+			});
+	},
+	async setUserProfile ({commit}, {id}) {
+		await this.$axios.$get(`/user-profiles?user.id=${id}`)
+			.then(response =>  {
+			// handle success
+				commit("SET_USERPROFILE", response);
+			})
+			.catch((e) => {
+			// handle error
+				// commit("SET_USERPROFILE", error);
+			})
+			.finally(function () {
+			// always executed
+				console.log('finally');
+			});
 	}
 };
-
-	
