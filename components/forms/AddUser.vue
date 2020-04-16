76510dca45e1769537eff7583bc6c7d3fc80df9d
Raja s <rajaaltus@gmail.com>
2020-04-16 17:55:43 +0700

    add user started

---
+++ components/forms/AddUser.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="text-center">
+    <v-dialog v-model="dialog" persistent max-width="600px">
+      <template v-slot:activator="{ on }">
+         <v-btn color="warning" fab dark v-on="on">
+      <v-icon>mdi-account-plus</v-icon>
+    </v-btn> 
+      </template>
+      <v-card>
+        <v-card-title>
+          <span class="headline">Create new user</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col cols="12">
+                <v-text-field v-model="fullname" label="Full name *" required outlined hint="Ex: Mr. Mighty Joe" prepend-inner-icon="mdi-account"></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field v-model="email" label="Email Address *" required hint="Primary login id" outlined  prepend-inner-icon="email"></v-text-field>
+              </v-col>
+            </v-row>
+          </v-container>
+          <small>*Both are required fields</small>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="dialog = false">Reset</v-btn>
+          <v-btn color="blue darken-1" text @click="dialog = false">Create</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+   
+  </div>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      dialog: false,
+      newUser: {
+
+      }
+    }),
+  }
+</script>
--- components/forms/ProgramForm.vue
+++ components/forms/ProgramForm.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <v-row no-gutters>
-      <v-col cols="12" md="12">
+      <v-col cols="11" lg="11">
         <v-select
           v-model="program.user"
           :items="dataFrom"
@@ -13,6 +13,9 @@
           color="success"
           :rules="[v => !!v || 'Item is required']"
         ></v-select>
+      </v-col>
+      <v-col cols="1" lg="1" sm="1">
+        <AddUser />
       </v-col>
     </v-row>
     <v-row>
@@ -224,8 +227,12 @@
 <script>
 import Swal from "sweetalert2";
 import { mapState } from "vuex";
+import AddUser from '@/components/forms/AddUser'
 export default {
   props: ["programNames", "dataFrom"],
+  components: {
+    AddUser
+  },
   data() {
     return {
       duration_from: false,
