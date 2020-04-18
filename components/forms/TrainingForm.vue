10f0b0a9cf3b62f2ff37688ddbec50752d71237e
Raja s <rajaaltus@gmail.com>
2020-04-18 07:03:20 +0700

    upto public form with integ

--- components/forms/TrainingForm.vue
+++ components/forms/TrainingForm.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     <v-row no-gutters>
-      <v-col cols="12" md="12">
+      <v-col cols="11" lg="11">
         <v-select
-          v-model="training.userProfileId"
-          :items="staffs"
+          v-model="training.user"
+          :items="dataFrom"
           item-value="id"
-          item-text="firstname"
+          item-text="fullname"
           outlined
           label="Data collected From?"
           placeholder="Select Faculty / Staff from the List"
@@ -14,26 +14,28 @@
           :rules="[v => !!v || 'Item is required']"
         ></v-select>
       </v-col>
+      <v-col cols="1" lg="1" sm="1">
+        <AddUser @new-user="getLatestUsers()" />
+      </v-col>
     </v-row>
     <v-row>
       <v-col cols="12" md="12">
         <v-form ref="form" v-model="valid" lazy-validation @submit.prevent>
-          <v-container>
             <v-row>
-              <v-col cols="4">
+              <v-col cols="12">
                 <v-text-field
-                  v-model="training.facultyName"
+                  v-model="training.faculty_name"
                   :rules="[v => !!v || 'Item is required']"
-                  label="Faculty Name"
+                  label="Faculty Name(s) *"
                   required
                 >
                 </v-text-field>
               </v-col>
-              <v-col cols="8">
+              <v-col cols="12">
                 <v-text-field
-                  v-model="training.trainingName"
+                  v-model="training.program_name"
                   :rules="[v => !!v || 'Item is required']"
-                  label="Name of the Program"
+                  label="Name of the Program *"
                   required
                 >
                 </v-text-field>
@@ -42,9 +44,9 @@
             <v-row>
               <v-col cols="12">
                 <v-text-field
-                  v-model="training.institutionAndPlace"
+                  v-model="training.institutional_affiliation"
                   :rules="[v => !!v || 'Item is required']"
-                  label="Institution &amp; Place"
+                  label="Institution &amp; Place *"
                   required
                 >
                 </v-text-field>
@@ -53,54 +55,78 @@
             <v-row>
               <v-col cols="4">
                 <v-menu
+                  ref="menu"
                   v-model="duration_from"
-                  :close-on-content-click="true"
+                  :close-on-content-click="false"
                   transition="scale-transition"
                   offset-y
                   min-width="290px"
                 >
                   <template v-slot:activator="{ on }">
                     <v-text-field
-                      v-model="training.fromDate"
-                      label="From Date"
-                      v-on="on"
+                      v-model="training.from_date"
+                      :return-value.sync="date"
                       :rules="[v => !!v || 'Item is required']"
+                      readonly
+                      label="From Date *"
+                      v-on="on"
                     ></v-text-field>
                   </template>
                   <v-date-picker
-                    v-model="training.fromDate"
+                    v-model="training.from_date"
                     color="green lighten-1"
-                    @input="menu1 = false"
-                  ></v-date-picker>
+                    no-title
+                    scrollable
+                  >
+                    <v-spacer></v-spacer>
+                    <v-btn text color="primary" @click="duration_from = false">
+                      Cancel
+                    </v-btn>
+                    <v-btn text color="primary" @click="$refs.menu.save(date)">
+                      OK
+                    </v-btn>
+                  </v-date-picker>
                 </v-menu>
               </v-col>
               <v-col cols="4">
                 <v-menu
+                  ref="menu1"
                   v-model="duration_to"
-                  :close-on-content-click="true"
+                  :close-on-content-click="false"
                   transition="scale-transition"
                   offset-y
                   min-width="290px"
                 >
                   <template v-slot:activator="{ on }">
                     <v-text-field
-                      v-model="training.toDate"
-                      label="To Date"
-                      v-on="on"
+                      v-model="training.to_date"
                       :rules="[v => !!v || 'Item is required']"
+                      :return-value.sync="date1"
+                      readonly
+                      label="To Date *"
+                      v-on="on"
                     ></v-text-field>
                   </template>
-                  <v-date-picker
-                    v-model="training.toDate"
-                    color="green lighten-1"
-                    @input="menu2 = false"
-                  ></v-date-picker>
+                  <v-date-picker v-model="training.to_date" no-title scrollable>
+                    <v-spacer></v-spacer>
+                    <v-btn text color="primary" @click="duration_to = false">
+                      Cancel
+                    </v-btn>
+                    <v-btn
+                      text
+                      color="primary"
+                      @click="$refs.menu1.save(date1)"
+                    >
+                      OK
+                    </v-btn>
+                  </v-date-picker>
                 </v-menu>
               </v-col>
               <v-col cols="4">
                 <v-text-field
-                  v-model="training.fundedBy"
-                  label="Funded By"
+                  v-model="training.funded_by"
+                  :rules="[v => !!v || 'Item is required']"
+                  label="Funded By *"
                   required
                 >
                 </v-text-field>
@@ -110,55 +136,156 @@
             <v-row>
               <v-container fluid>
                 <v-textarea
-                  v-model="training.briefReport"
-                  counter
-                  label="Brief Report"
-                  :value="value"
+                  v-model="training.brief_report"
+                  counter="1000"
+                  maxlength="1000"
+                  :rules="[v => !!v || 'Item is required']"
+                  hint="Only 1000 Characters are allowed."
+                  label="Brief Report *"
                 ></v-textarea>
+                <span style="color:red; font-size:12px;"
+                  >* Mandatory fields</span
+                >
               </v-container>
             </v-row>
-          </v-container>
+            <v-row>
+              <v-col cols="12" lg="4" md="12">
+              <h3><span class="frm-title">Upload Images (If any)</span></h3>
+              <v-hover>
+                <template v-slot:default="{ hover }">
+                  <v-img
+                    :src="`${$axios.defaults.baseURL}${image_url}`"
+                    lazy-src="/image_placeholder.png"
+                    class="mt-3"
+                    max-width="100%"
+                    max-height="175"
+                  >
+                    <v-fade-transition>
+                      <v-overlay v-if="hover" absolute color="#00564c">
+                        <v-btn @click="$refs.image.click()">
+                          Upload Image
+                        </v-btn>
+                      </v-overlay>
+                    </v-fade-transition>
+                  </v-img>
+                </template>
+              </v-hover>
+              <input
+                ref="image"
+                type="file"
+                style="display:none;"
+                label="File input"
+                @change="handleFileUpload"
+              />
+            </v-col>
+            </v-row>
+          <v-row>
+            <v-spacer></v-spacer>
+            <v-btn small color="#d74f4f" dark @click="reset" class="mr-4">
+              Reset
+            </v-btn>
+            <v-btn small color="#57a727" dark @click="trainingAdd" class="mr-4">
+              Submit
+            </v-btn>
+          </v-row>
         </v-form>
       </v-col>
-    </v-row>
-    <v-row>
-      <v-spacer></v-spacer>
-      <v-btn small color="#d74f4f" dark @click="reset" class="mr-4">
-        Reset
-      </v-btn>
-      <v-btn small color="#57a727" dark @click="programAdd" class="mr-4">
-        Submit
-      </v-btn>
     </v-row>
   </div>
 </template>
 
 <script>
+import Swal from "sweetalert2";
+import { mapState } from "vuex";
+import AddUser from "@/components/forms/AddUser";
 export default {
+  props: ["dataFrom"],
+  components: {
+    AddUser
+  },
   data: () => ({
+    dataFrom: [],
     loading: false,
     duration_from: false,
     duration_to: false,
     valid: false,
-    training: {
-      annualYear: 0,
-      approvalStatus: "PENDING",
-      approvedBy: "",
-      approvedDate: "",
-      briefReport: "",
-      departmentId: 0,
-      facultyName: "",
-      fromDate: new Date().toISOString().substr(0, 10),
-      fundedBy: "",
-      institutionAndPlace: "",
-      rejectedReason: "",
-      status: "VALID",
-      toDate: new Date().toISOString().substr(0, 10),
-      trainingName: "",
-      userProfileId: 0
+    training: 
+		{
+			annual_year: 0,
+			faculty_name: "",
+			program_name: "",
+			institutional_affiliation: "",
+			from_date: "",
+			to_date: "",
+			funded_by: "",
+			brief_report: "",
+			approval_status: "Pending",
+			approved_by: null,
+			approved_date: null,
+      deleted: false,
+      image: null,
+			department: 0,
+			user: 0
     },
-    approvals: ["PENDING", "REJECTED", "APPROVED"]
-  })
+    selectedFile: null,
+    image_url: null,
+  }),
+  methods: {
+    getLatestUsers() {
+      console.log("recieving....");
+      let queryString = "";
+      queryString = `department.id=${this.$store.state.auth.user.department}&userType=FACULTY&blocked_ne=true`;
+      this.$store.dispatch("setStaffs", { qs: queryString });
+      this.dataFrom = this.$store.state.staffs;
+    },
+    reset() {
+      this.$refs.form.reset();
+      this.image_url = null;
+    },
+    async trainingAdd () {
+			if (this.$refs.form.validate()) {
+				this.training.annual_year = this.$store.state.selectedYear;
+				if (this.training.user == 0)
+					this.training.user = this.$store.state.auth.user.id;
+				if(this.$store.state.auth.user.userType==='DEPARTMENT') {
+					var today = new Date();
+          this.training.approved_date = this.$moment(today).format();
+          this.training.approval_status = 'Approved';
+				}
+				this.training.department = this.$store.state.auth.user.department;
+				var payload = this.training;
+				//  console.log(payload);
+				this.$store.dispatch('training/addTraining', payload)
+					.then(resp => {
+						Swal.fire({
+								
+							title: 'Success',
+							text: 'Added Successfully!',
+							icon: 'success',
+							showConfirmButton: false,
+							timer: 1500
+						})
+						this.reset();
+					})
+					.catch(err => {
+						Swal.fire('Something Wrong!')
+					});
+			 }
+		},
+    async handleFileUpload(event) {
+      this.selectedFile = event.target.files[0];
+      // console.log(this.selectedFile);
+      const data = new FormData();
+      data.append("files", this.selectedFile);
+      const uploadRes = await this.$axios({
+        method: "POST",
+        url: "/upload",
+        data
+      });
+      this.image_url = uploadRes.data[0].url;
+      this.training.image = uploadRes.data[0].id;
+    }
+  }
 };
 </script>
 
--- components/forms/VisitorForm.vue
+++ components/forms/VisitorForm.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <v-row no-gutters>
-      <v-col cols="12" md="12">
+      <v-col cols="11" lg="11">
         <v-select
           v-model="visitor.user"
           :items="dataFrom"
@@ -14,132 +14,192 @@
           :rules="[v => !!v || 'Item is required']"
         ></v-select>
       </v-col>
+      <v-col cols="1" lg="1" sm="1">
+        <AddUser @new-user="getLatestUsers()" />
+      </v-col>
     </v-row>
     <v-row>
       <v-col cols="12" md="12">
         <v-form ref="form" v-model="valid" lazy-validation @submit.prevent>
-          <v-container>
-            <v-row>
-              <v-col cols="4">
-                <v-text-field
-                  color="green"
-                  v-model="visitor.name"
-                  label="Visitor Name"
-                  :rules="[v => !!v || 'Item is required']"
+          <v-row>
+            <v-col cols="4">
+              <v-text-field
+                v-model="visitor.name"
+                label="Visitor Name *"
+                :rules="[v => !!v || 'Item is required']"
+                required
+              >
+              </v-text-field>
+            </v-col>
+            <v-col cols="4">
+              <v-text-field
+                v-model="visitor.designation"
+                label="Designation *"
+                :rules="[v => !!v || 'Item is required']"
+                required
+              >
+              </v-text-field>
+            </v-col>
+            <v-col cols="4">
+              <v-text-field
+                v-model="visitor.institutional_affiliation"
+                label="Institute Affiliation *"
+                :rules="[v => !!v || 'Item is required']"
+                required
+              >
+              </v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="12">
+              <v-text-field
+                v-model="visitor.title"
+                label="Lecture Title *"
+                :rules="[v => !!v || 'Item is required']"
+                required
+              >
+              </v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="6">
+              <v-menu
+                ref="menu"
+                v-model="duration_from"
+                :close-on-content-click="false"
+                transition="scale-transition"
+                offset-y
+                min-width="290px"
+              >
+                <template v-slot:activator="{ on }">
+                  <v-text-field
+                    v-model="visitor.from_date"
+                    :return-value.sync="date"
+                    :rules="[v => !!v || 'Item is required']"
+                    readonly
+                    label="From Date *"
+                    v-on="on"
+                  ></v-text-field>
+                </template>
+                <v-date-picker
+                  v-model="visitor.from_date"
+                  color="green lighten-1"
+                  no-title
+                  scrollable
                 >
-                </v-text-field>
-              </v-col>
-              <v-col cols="4">
-                <v-text-field
-                  color="green"
-                  v-model="visitor.designation"
-                  label="Designation"
-                  :rules="[v => !!v || 'Item is required']"
-                >
-                </v-text-field>
-              </v-col>
-              <v-col cols="4">
-                <v-text-field
-                  color="green"
-                  v-model="visitor.institutionAffiliation"
-                  label="Institute Affiliation"
-                  :rules="[v => !!v || 'Item is required']"
-                >
-                </v-text-field>
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-col cols="12">
-                <v-text-field
-                  color="green"
-                  v-model="visitor.lectureTitle"
-                  label="Lecture Title"
-                  :rules="[v => !!v || 'Item is required']"
-                >
-                </v-text-field>
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-col cols="6">
-                <v-menu
-                  v-model="duration_from"
-                  :close-on-content-click="true"
-                  :nudge-right="40"
-                  transition="scale-transition"
-                  offset-y
-                  min-width="290px"
-                >
-                  <template v-slot:activator="{ on }">
-                    <v-text-field
-                      color="green"
-                      v-model="visitor.fromDate"
-                      label="From Date"
-                      v-on="on"
-                    ></v-text-field>
-                  </template>
-                  <v-date-picker
-                    v-model="visitor.fromDate"
-                    color="green lighten-1"
-                    @input="menu1 = false"
-                  ></v-date-picker>
-                </v-menu>
-              </v-col>
-              <v-col cols="6">
-                <v-menu
-                  v-model="duration_to"
-                  :close-on-content-click="true"
-                  :nudge-right="40"
-                  transition="scale-transition"
-                  offset-y
-                  min-width="290px"
-                >
-                  <template v-slot:activator="{ on }">
-                    <v-text-field
-                      color="green"
-                      v-model="visitor.toDate"
-                      label="To Date"
-                      v-on="on"
-                    ></v-text-field>
-                  </template>
-                  <v-date-picker
-                    v-model="visitor.toDate"
-                    color="green lighten-1"
-                    @input="menu2 = false"
-                  ></v-date-picker>
-                </v-menu>
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-container fluid>
-                <v-textarea
-                  v-model="visitor.briefReport"
-                  counter="250"
-                  label="Brief Report"
-                  :value="value"
-                ></v-textarea>
-              </v-container>
-            </v-row>
-          </v-container>
+                  <v-spacer></v-spacer>
+                  <v-btn text color="primary" @click="duration_from = false">
+                    Cancel
+                  </v-btn>
+                  <v-btn text color="primary" @click="$refs.menu.save(date)">
+                    OK
+                  </v-btn>
+                </v-date-picker>
+              </v-menu>
+            </v-col>
+            <v-col cols="6">
+              <v-menu
+                ref="menu1"
+                v-model="duration_to"
+                :close-on-content-click="false"
+                transition="scale-transition"
+                offset-y
+                min-width="290px"
+              >
+                <template v-slot:activator="{ on }">
+                  <v-text-field
+                    v-model="visitor.to_date"
+                    :rules="[v => !!v || 'Item is required']"
+                    :return-value.sync="date1"
+                    readonly
+                    label="To Date *"
+                    v-on="on"
+                  ></v-text-field>
+                </template>
+                <v-date-picker v-model="visitor.to_date" no-title scrollable>
+                  <v-spacer></v-spacer>
+                  <v-btn text color="primary" @click="duration_to = false">
+                    Cancel
+                  </v-btn>
+                  <v-btn text color="primary" @click="$refs.menu1.save(date1)">
+                    OK
+                  </v-btn>
+                </v-date-picker>
+              </v-menu>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-container fluid>
+              <v-textarea
+                v-model="visitor.brief_report"
+                counter="1000"
+                maxlength="1000"
+                :rules="[v => !!v || 'Item is required']"
+                hint="Only 1000 Characters are allowed."
+                label="Brief Report *"
+              ></v-textarea>
+              <span style="color:red; font-size:12px;">* Mandatory fields</span>
+            </v-container>
+          </v-row>
+          <v-row>
+            <v-col cols="12" lg="4">
+              <h3><span class="frm-title">Upload Images (If any)</span></h3>
+              <!-- <input type="file" style="display:none;" label="File input" ref="image"  @change="handleFileUpload"> -->
+              <v-hover>
+                <template v-slot:default="{ hover }">
+                  <v-img
+                    :src="`${$axios.defaults.baseURL}${image_url}`"
+                    lazy-src="/image_placeholder.png"
+                    class="mt-3"
+                    max-width="100%"
+                    max-height="175"
+                  >
+                    <v-fade-transition>
+                      <v-overlay v-if="hover" absolute color="#00564c">
+                        <v-btn @click="$refs.image.click()">
+                          Upload Image
+                        </v-btn>
+                      </v-overlay>
+                    </v-fade-transition>
+                  </v-img>
+                </template>
+              </v-hover>
+              <input
+                ref="image"
+                type="file"
+                style="display:none;"
+                label="File input"
+                @change="handleFileUpload"
+              />
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-spacer></v-spacer>
+            <v-btn small color="#d74f4f" dark @click="reset" class="mr-4">
+              Reset
+            </v-btn>
+            <v-btn small color="#57a727" dark @click="visitorAdd" class="mr-4">
+              Submit
+            </v-btn>
+          </v-row>
         </v-form>
       </v-col>
-    </v-row>
-    <v-row>
-      <v-spacer></v-spacer>
-      <v-btn small color="#d74f4f" dark @click="reset" class="mr-4">
-        Reset
-      </v-btn>
-      <v-btn small color="#57a727" dark @click="programAdd" class="mr-4">
-        Submit
-      </v-btn>
     </v-row>
   </div>
 </template>
 
 <script>
+import Swal from "sweetalert2";
+import { mapState } from "vuex";
+import AddUser from "@/components/forms/AddUser";
 export default {
   props: ["dataFrom"],
+  components: {
+    AddUser
+  },
   data() {
     return {
+      dataFrom: [],
       duration_from: false,
       duration_to: false,
       valid: true,
@@ -152,16 +212,81 @@ export default {
         from_date: "",
         to_date: "",
         brief_report: "",
-        approval_status: "",
+        approval_status: "Pending",
         approved_by: "",
         approved_date: null,
         deleted: false,
         department: 0,
         user: 0,
+        image: null,
         rejected_reason: null
       },
-      approvals: ["Pending", "Rejected", "Approved"]
+      approvals: ["Pending", "Rejected", "Approved"],
+      selectedFile: null,
+      image_url: null
     };
+  },
+  methods: {
+    getLatestUsers() {
+      console.log("recieving....");
+      let queryString = "";
+      queryString = `department.id=${this.$store.state.auth.user.department}&userType=FACULTY&blocked_ne=true`;
+      this.$store.dispatch("setStaffs", { qs: queryString });
+      this.dataFrom = this.$store.state.staffs;
+    },
+    reset() {
+      this.$refs.form.reset();
+      this.image_url = null;
+    },
+    async handleFileUpload(event) {
+      this.selectedFile = event.target.files[0];
+      // console.log(this.selectedFile);
+      const data = new FormData();
+      data.append("files", this.selectedFile);
+      const uploadRes = await this.$axios({
+        method: "POST",
+        url: "/upload",
+        data
+      });
+      this.image_url = uploadRes.data[0].url;
+      this.visitor.image = uploadRes.data[0].id;
+    },
+    async visitorAdd() {
+      if (this.$refs.form.validate()) {
+        this.visitor.annual_year = this.$store.state.selectedYear;
+        if (this.$store.state.auth.user.userType === "DEPARTMENT") {
+          var today = new Date();
+          this.visitor.approved_date = this.$moment(today).format();
+          this.visitor.approval_status = "Approved";
+        }
+        if (this.visitor.user == 0)
+          this.visitor.user = this.$store.state.auth.user.id;
+        this.visitor.department = this.$store.state.auth.user.department;
+        var payload = this.visitor;
+        // console.log(payload);
+        this.$store
+          .dispatch("visitor/addVisitor", payload)
+          .then(resp => {
+            Swal.fire({
+              title: "Success",
+              text: "Added Successfully!",
+              icon: "success",
+              showConfirmButton: false,
+              timer: 1500
+            });
+            this.reset();
+          })
+          .catch(err => {
+            Swal.fire({
+              title: "Oops!",
+              text: 'Someting Wrong!',
+              icon: "error",
+              showConfirmButton: false,
+              timer: 3000,
+            })
+          });
+      }
+    }
   }
 };
 </script>
