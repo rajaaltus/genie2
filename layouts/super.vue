7c4fe0c7bb9bc42a0b70d290339788f9bcaadf76
Raja s <rajaaltus@gmail.com>
2020-05-10 20:46:05 +0700

    report duplication fixed

--- layouts/super.vue
+++ layouts/super.vue
@@ -31,7 +31,7 @@
       <v-divider></v-divider>
 
       <v-list dense>
-        <v-list-item v-for="item in items" :key="item.title" link>
+        <v-list-item :to="item.to" router exact v-for="(item,index) in items" :key="index" link>
           <v-list-item-icon>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-icon>
