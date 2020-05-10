fe9cc76dc3805b9ceab2343ce6469ea24eb33f22
Raja s <rajaaltus@gmail.com>
2020-05-10 09:22:35 +0700

    saved reports done

--- components/AvailableReports.vue
+++ components/AvailableReports.vue
@@ -2,63 +2,112 @@
   <div class="text-center">
     <v-bottom-sheet v-model="sheet" inset>
       <v-list>
-        <v-subheader>Open in</v-subheader>
+        <v-subheader>Saved Reports</v-subheader>
         <v-list-item
           v-for="tile in availableReports"
           :key="tile.id"
-          @click="sheet = false"
+          @click="handleClick(tile.id, tile.userType)"
         >
           <v-list-item-avatar>
             <v-avatar size="32px" tile>
-
               <img
                 :src="`https://cdn.vuetifyjs.com/images/bottom-sheets/${tiles[0].img}`"
                 :alt="tile.annual_year"
-              >
+              />
             </v-avatar>
           </v-list-item-avatar>
-          <v-list-item-title>{{ tile.annual_year }} - {{tile.userType}}</v-list-item-title>
+          <v-list-item-title
+            >{{ tile.annual_year }} - {{ tile.userType }}</v-list-item-title
+          >
         </v-list-item>
       </v-list>
+       <div id="savedDoc" v-html="formattedDoc" style="display:none"></div>
     </v-bottom-sheet>
   </div>
 </template>
 
 <script>
-  export default {
-    props: ['availableReports'],
-    data: () => ({
-      sheet: true,
-      tiles: [
-        
-        { img: 'keep.png', title: 'Keep' },
-        { img: 'inbox.png', title: 'Inbox' },
-        { img: 'hangouts.png', title: 'Hangouts' },
-        { img: 'messenger.png', title: 'Messenger' },
-        { img: 'google.png', title: 'Google+' },
-      ],
+import {mapState} from 'vuex'
+export default {
+  props: ['availableReports'],
+  data: () => ({
+    sheet: true,
+    tiles: [
+
+      { img: 'keep.png', title: 'Keep' },
+      { img: 'inbox.png', title: 'Inbox' },
+      { img: 'hangouts.png', title: 'Hangouts' },
+      { img: 'messenger.png', title: 'Messenger' },
+      { img: 'google.png', title: 'Google+' },
+    ],
+  }),
+  computed: {
+    ...mapState({
+      savedDoc: state => state.report.generatedReport
     }),
-    computed: {
-      // availableReports() {
-      //   return this.$store.state.report.availableReports;
-      // },
-      // sheet:{
-      //   get() {
-      //     return this.report;
-      //   },
-      //   set(val) {
-      //     return this.report=!val;
-      //     this.$emit="closed"
-      //   }
-      // }
+    formattedDoc() {
+      var final = '';
+      final = this.savedDoc.section_a;
+      if(this.savedDoc.section_b)
+        final += this.savedDoc.section_b
+      if(this.savedDoc.section_c)
+        final += this.savedDoc.section_c
+      if(this.savedDoc.section_d)
+        final += this.savedDoc.section_d
+      if(this.savedDoc.section_e)
+        final += this.savedDoc.section_e
+      if(this.savedDoc.section_f)
+        final += this.savedDoc.section_f
+      return final;
+    }
+  },
+  methods: {
+    async handleClick(id,userType) {
+      await this.$store.dispatch('report/getById', {id: id})
+      this.exportToDoc(`Saved-${userType}`)
+
     },
-    // async created() {
-    //   if(this.$store.state.report.availableReports)
-    //     this.sheet = true;
-    // },
-    // mounted() {
-    //   // if(this.$store.state.report.availableReports)
-    //     this.sheet = true;
-    // },
+    exportToDoc(filename = "") {
+    var preHtml =
+      "<html xmlns:o='urn:schemas-microsoft-com:office:office' xmlns:w='urn:schemas-microsoft-com:office:word' xmlns='http://www.w3.org/TR/REC-html40'><head><meta charset='utf-8'><title>Export HTML To Doc</title></head><body>";
+    var postHtml = "</body></html>";
+    var html =
+      // preHtml + document.getElementsByClassName('ProseMirror')[0].innerHTML + postHtml;
+      preHtml + document.getElementById("savedDoc").innerHTML + postHtml;
+
+    var blob = new Blob(["\ufeff", html], {
+      type: "application/msword",
+    });
+
+    // Specify link url
+    var url =
+      "data:application/vnd.ms-word;charset=utf-8," +
+      encodeURIComponent(html);
+
+    // Specify file name
+    filename = filename ? filename + ".doc" : "document.doc";
+
+    // Create download link element
+    var downloadLink = document.createElement("a");
+
+    document.body.appendChild(downloadLink);
+
+    if (navigator.msSaveOrOpenBlob) {
+      navigator.msSaveOrOpenBlob(blob, filename);
+    } else {
+      // Create a link to the file
+      downloadLink.href = url;
+
+      // Setting the file name
+      downloadLink.download = filename;
+
+      //triggering the function
+      downloadLink.click();
+    }
+
+    document.body.removeChild(downloadLink);
+  },
   }
+
+}
 </script>
